5a29852522c545e1b7e306bda9dcd1059be02a64
pb1983 <phil.bryer@gmail.com>
2023-08-19 18:22:20 -0400

    finished constructing classes for the SVG to generate with text, background color, shape, and text color. Created prompt to receive these datapoints from the user

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const fs = require("fs")
 const SVG = require("./svg.js")
 
-let example = new SVG("blue", "green", "PJB", "triangle");
+
+let example = new SVG("tan", "brown", "KFO", "square");
 
 fs.writeFile("shape.svg", example.shape, function(err){
     if(err)
--- svg.js
+++ svg.js
@@ -1,57 +1,50 @@
-const Shape = require("./lib/shapes.js");
-const Circle = require("./lib/shapes.js");
-const Square = require("./lib/shapes.js");
-const Triangle = require("./lib/shapes.js");
+const { Shape, Circle, Square, Triangle } = require("./lib/shapes.js");
 
 
 
 
 class SVG extends Shape {
-    constructor(fill, textColor, text, shape) {
-     super(fill, textColor, text)
-        this.shape = shape;
-    
+   constructor(fill, textColor, text, shape) {
+      super(fill, textColor, text)
+      this.shape = shape;
+      
 
-             if (shape === "triangle") {
-            
-                    
 
-                `<svg version="1.1"
-                width="300" height="200"
-                xmlns="http://www.w3.org/2000/svg">
-           
-                ${Triangle.render()}
-             
+      if (shape === "triangle") {
+         const triangle = new Triangle(fill, textColor, text)
+         this.shape = 
+            `<svg version="1.1"
+                    width="300" height="200"
+                    xmlns="http://www.w3.org/2000/svg">
+                     ${triangle.render()}
+                             
                </svg>`
-                    return
-             } else if (shape === "circle") {
-            
-        
-                `<svg version="1.1"
-                width="300" height="200"
-                xmlns="http://www.w3.org/2000/svg">
-           
-                ${Circle.render}
-             
+      }
+
+      if (shape === "circle") {
+
+         const circle = new Circle(fill, textColor, text)
+         this.shape = `<svg version="1.1"
+                     width="300" height="200"
+                     xmlns="http://www.w3.org/2000/svg">
+                     ${circle.render()}
+                              
                </svg>`
-             } else if (shape === "square") {
-            
-          
-        
-                `<svg version="1.1"
-                width="300" height="200"
-                xmlns="http://www.w3.org/2000/svg">
-           
-                ${Square.render}
-             
-               </svg>`
-             }
-            }
-    }
+      }
 
 
-newSVG = new SVG("blue", "green", "PJB", "triangle")
+      if (shape === "square") {
+         const square = new Square(fill,textColor, text)
+         this.shape = `<svg version="1.1"
+                        width="300" height="200"
+                        xmlns="http://www.w3.org/2000/svg">
+                        ${square.render()}
+                                 
+                  </svg>`
+      }
+   
+   }
+}
 
-console.log(newSVG)
 
 module.exports = SVG;
